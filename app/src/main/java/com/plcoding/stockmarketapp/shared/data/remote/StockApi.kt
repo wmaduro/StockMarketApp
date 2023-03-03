fc31f544f6d4da9c4cab506212d147013c007c94
Welerson Maduro <welerson.maduro@foodstuffs-si.co.nz>
2023-03-03 16:46:44 +1300

    api key

--- app/src/main/java/com/plcoding/stockmarketapp/shared/data/remote/StockApi.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/shared/data/remote/StockApi.kt
@@ -25,7 +25,7 @@ interface StockApi {
     ): CompanyInfoDto
 
     companion object {
-        const val API_KEY = "G1USXWKX272RK4BP"
+        const val API_KEY = "8ABSKZDIVWRXYT96"
         const val BASE_URL = "https://alphavantage.co"
     }
 }
